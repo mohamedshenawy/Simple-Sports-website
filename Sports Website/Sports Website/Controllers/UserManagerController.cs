9d6ba6a54af6d7f60a2c4f70f6102657569e7a41
mohamed shenawy <mohamedshenawy447@gmail.com>
2022-12-24 01:15:53 +0200

    edit user

--- Sports Website/Sports Website/Controllers/UserManagerController.cs
+++ Sports Website/Sports Website/Controllers/UserManagerController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ViewModels;
 
 namespace Sports_Website.Controllers
 {
@@ -31,8 +32,44 @@ namespace Sports_Website.Controllers
                 return BadRequest(e.Message);
             }
         }
-        //insert 
+        //insert
         //update
+        [HttpGet]
+        public async Task<IActionResult> Update(string userId)
+        {
+            try
+            {
+                //var user = _userManager.Users.Where(user => user.Id == userId).First();
+                var user = await _userManager.FindByIdAsync(userId);
+                return View(user);
+
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateUser([FromBody]UpdateUserVM model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var user = await _userManager.FindByIdAsync(model.Id);
+                user.UserName = model.Name;
+                user.NormalizedUserName = model.Name.ToUpper();
+                user.Email = model.Email;
+                user.NormalizedEmail = model.Email.ToUpper();
+                var result = await _userManager.UpdateAsync(user);
+                if (!result.Succeeded)
+                    return BadRequest("update user failed");
+                return View("Index",_userManager.Users);
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
         //delete
 
     }
---
+++ Sports Website/ViewModels/UpdateUserVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ViewModels
+{
+    public class UpdateUserVM
+    {
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Emial not valid")]
+        public string Email { get; set; }
+
+    }
+}
