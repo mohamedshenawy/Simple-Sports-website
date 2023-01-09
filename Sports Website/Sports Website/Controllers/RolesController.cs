d6d1677ceb2e7f93c053dfd35d7e0dbef8d7c090
mohamed shenawy <mohamedshenawy447@gmail.com>
2023-01-09 22:37:28 +0200

    asdfasfas

--- Sports Website/Sports Website/Controllers/RolesController.cs
+++ Sports Website/Sports Website/Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using ViewModels;
 
 namespace Sports_Website.Controllers
 {
+    [Authorize(Roles ="super admin,admin")]
     public class RolesController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
